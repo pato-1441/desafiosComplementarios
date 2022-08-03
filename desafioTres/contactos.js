5ce26b6be0d130959e84e9ba6d518e2ce3fc133a
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-08-03 17:31:49 -0300

    upload Desafio Tres

---
+++ desafioTres/contactos.js
@@ -0,0 +1,16 @@
+//
+//
+//
+//
+//
+class User {
+    constructor(fname,lname,mail,password,birth){
+        this.fname = fname;
+        this.lname = lname;
+        this.mail = mail;
+        this.password = password;
+        this.birth = birth;
+    }
+}
+
+const admin = new User('Patricio','Albornoz','admin@gmail.com','admin','16/05/2004');

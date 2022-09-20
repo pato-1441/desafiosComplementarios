065e32de19cb5f0fdd1c802b3853d27d38420abd
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-09-20 19:10:16 -0300

    trabjando en desafio manejo de archivos

--- desafioArchivos/class.js
+++ desafioArchivos/class.js
@@ -8,15 +8,21 @@ class Contenedor{
     async save(object){
         const productos = await this.getAll();
         console.log(productos);
-        //const producto = object;
+        const producto = object;
+        console.log(producto);
         //producto.id = productos?.id === 0 ? 1 : productos[productos.length-1].id+1;
 
         //productos.push(object);
-        //await fs.promises.writeFile(this.nombreArchivo,JSON.stringify(object),'utf-8');
+        await fs.promises.writeFile(this.nombreArchivo,JSON.stringify(producto),'utf-8');
     }
 
     async getById(id){
-        
+        const productos = await this.getAll();
+        //console.log(productos);
+        const resultado = productos.find(producto=>producto.id===id);
+        //resultado && (return resultado;)
+        //const encontrado = productos.find(producto=>{producto.id});
+        //return encontrado;
     }
 
     async getAll(){
@@ -26,7 +32,7 @@ class Contenedor{
                 const files = JSON.parse(file);
                 return files
             } else {
-                return [];
+                return `No existen productos en el archivo: "${this.nombreArchivo}"`;
             }
         } catch (error) {
             console.log(`Error: ${error}`);
@@ -36,8 +42,13 @@ class Contenedor{
 
 const prueba = new Contenedor('./archivo.txt');
 prueba.getAll();
-prueba.save({
+prueba.save([{
+    "id":"0",
     "title": "Coca-Cola",
     "price": "550",
     "thumbnail": "empty"
-  });
+  },{"id":"1",
+  "title": "Pepsi",
+  "price": "350",
+  "thumbnail": "empty"}]);
+prueba.getById(0);

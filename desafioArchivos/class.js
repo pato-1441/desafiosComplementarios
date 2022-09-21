75e4f33cd6652bf7923a9e7a9256e1f558e392ab
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-09-20 22:31:07 -0300

    ya casi terminado

--- desafioArchivos/class.js
+++ desafioArchivos/class.js
@@ -5,25 +5,32 @@ class Contenedor{
     constructor(nombreArchivo) {
         this.nombreArchivo=nombreArchivo;
     }
-    async save(object){
-        const productos = await this.getAll();
-        console.log(productos);
-        const producto = object;
-        console.log(producto);
-        //producto.id = productos?.id === 0 ? 1 : productos[productos.length-1].id+1;
 
-        //productos.push(object);
-        await fs.promises.writeFile(this.nombreArchivo,JSON.stringify(producto),'utf-8');
+    async save(object){
+        try {
+            const productos = await this.getAll();
+            const productosDos = productos.concat(object);
+            console.log(productosDos);
+            await fs.promises.writeFile(this.nombreArchivo,JSON.stringify((productosDos),null,2),'utf-8');
+            console.log(`Escritura realizada con éxito.`)
+        } catch (error) {
+            throw new Error (`Error en escritura: ${error.message}`);
+        }
+        
     }
 
     async getById(id){
         const productos = await this.getAll();
-        //console.log(productos);
-        const resultado = productos.find(producto=>producto.id===id);
-        //resultado && (return resultado;)
-        //const encontrado = productos.find(producto=>{producto.id});
-        //return encontrado;
-    }
+        try {
+            productos.forEach(producto => {
+                if(producto.id===id){
+                    console.log(`\n Producto seleccionado: ${producto.title}\n ID: ${producto.id}\n Precio: $${producto.price}`)
+                }
+            });
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }        
+    };
 
     async getAll(){
         try {
@@ -32,23 +39,77 @@ class Contenedor{
                 const files = JSON.parse(file);
                 return files
             } else {
-                return `No existen productos en el archivo: "${this.nombreArchivo}"`;
+                return [];
             }
         } catch (error) {
             console.log(`Error: ${error}`);
         }
     }
+
+    async deleteById(id){
+        const productos = await this.getAll();
+        try {
+            productos.forEach(producto => {
+                if(producto.id==id){
+                    //productos[producto.id]='';
+                    productos.splice(producto.id-1,1);
+                }
+            });
+            /* const item = productos.find((prod) => prod.id == id);
+            const indice = productos.indexOf(item);
+            productos.splice(indice, 1); */
+            await fs.promises.writeFile(this.nombreArchivo,JSON.stringify((productos),null,2),'utf-8');
+        } catch (error) {
+            console.log(`Error: ${error}`);
+        }  
+    }
 }
 
 const prueba = new Contenedor('./archivo.txt');
-prueba.getAll();
-prueba.save([{
-    "id":"0",
-    "title": "Coca-Cola",
-    "price": "550",
-    "thumbnail": "empty"
-  },{"id":"1",
-  "title": "Pepsi",
-  "price": "350",
-  "thumbnail": "empty"}]);
+//prueba.getAll();
+
+/* prueba.save([
+             {
+              id:"0",
+              title: "Coca-Cola",
+              price: "150",
+              thumbnail: "empty"
+              },{
+              id:"1",
+              title: "Pepsi",
+              price: "170",
+              thumbnail: "empty"
+              },{
+              id:"2",
+              title: "Sprite",
+              price: "200",
+              thumbnail: "empty"
+             }
+            ]);    
+ */
 prueba.getById(0);
+prueba.deleteById(0);
+prueba.getAll();
+
+
+
+/*
+prueba.save([
+             {
+              id:"3",
+              title: "Coca-Cola",
+              price: "150",
+              thumbnail: "empty"
+              },{
+              id:"4",
+              title: "Pepsi",
+              price: "170",
+              thumbnail: "empty"
+              },{
+              id:"5",
+              title: "Sprite",
+              price: "200",
+              thumbnail: "empty"
+             }
+            ]);    
+*/

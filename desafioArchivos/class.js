f58b56cb578434e3c91201c521a36b3cd97a623e
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-09-21 09:25:49 -0300

    actualizacion con correcciones, desafio terminado

--- desafioArchivos/class.js
+++ desafioArchivos/class.js
@@ -9,8 +9,12 @@ class Contenedor{
     async save(object){
         try {
             const productos = await this.getAll();
-            const productosDos = productos.concat(object);
-            console.log(productosDos);
+            let lastId = 0
+            if(productos.length)
+                lastId = productos[productos.length - 1].id
+            this.id = lastId + 1
+            const newObject = {...object, id: this.id}
+            const productosDos = productos.concat(newObject);
             await fs.promises.writeFile(this.nombreArchivo,JSON.stringify((productosDos),null,2),'utf-8');
             console.log(`Escritura realizada con éxito.`)
         } catch (error) {
@@ -22,14 +26,14 @@ class Contenedor{
     async getById(id){
         const productos = await this.getAll();
         try {
-            productos.forEach(producto => {
-                if(producto.id===id){
-                    console.log(`\n Producto seleccionado: ${producto.title}\n ID: ${producto.id}\n Precio: $${producto.price}`)
-                }
-            });
+            const arrayFound = productos.filter(producto => producto.id === id)
+            if(Array.isArray(arrayFound) && arrayFound.length)
+                return arrayFound[0]
+            else    
+                throw new Error(`Document with id ${id} not found`)
         } catch (error) {
-            console.log(`Error: ${error}`);
-        }        
+            console.log(`Error: ${error.message}`);
+        }         
     };
 
     async getAll(){
@@ -49,67 +53,41 @@ class Contenedor{
     async deleteById(id){
         const productos = await this.getAll();
         try {
-            productos.forEach(producto => {
-                if(producto.id==id){
-                    //productos[producto.id]='';
-                    productos.splice(producto.id-1,1);
-                }
-            });
-            /* const item = productos.find((prod) => prod.id == id);
-            const indice = productos.indexOf(item);
-            productos.splice(indice, 1); */
+            const matchIndex = productos.findIndex(producto => producto.id == id)
+            if(matchIndex=== -1)
+                throw new Error("Id not found")
+            console.log("🚀 ~ file: class.js ~ line 52 ~ Contenedor ~ deleteById ~ matchIndex", matchIndex)
+            productos.splice(matchIndex, 1)
             await fs.promises.writeFile(this.nombreArchivo,JSON.stringify((productos),null,2),'utf-8');
         } catch (error) {
-            console.log(`Error: ${error}`);
+            console.log(`Error: ${error.message}`);
         }  
+    }
+
+    async deleteAll(){
+        try {
+            await fs.promises.writeFile(this.nombreArchivo,(''),'utf-8');
+        } catch (error) {
+            throw new Error (`Error en escritura: ${error.message}`);
+        }
     }
 }
 
 const prueba = new Contenedor('./archivo.txt');
-//prueba.getAll();
 
-/* prueba.save([
-             {
-              id:"0",
-              title: "Coca-Cola",
-              price: "150",
-              thumbnail: "empty"
-              },{
-              id:"1",
-              title: "Pepsi",
-              price: "170",
-              thumbnail: "empty"
-              },{
-              id:"2",
-              title: "Sprite",
-              price: "200",
-              thumbnail: "empty"
-             }
-            ]);    
- */
-prueba.getById(0);
-prueba.deleteById(0);
-prueba.getAll();
+const main = async () => {
+    // prueba.getAll();
+    await prueba.save({
+        id: 2,
+        title: "Corona",
+        price: "375",
+        thumbnail: "empty"
+        }); 
+    console.log("Documento hallado: ", await prueba.getById(1));
+    await prueba.deleteById(0);
+    // await prueba.deleteAll();
+    // prueba.getAll();
+}
 
 
-
-/*
-prueba.save([
-             {
-              id:"3",
-              title: "Coca-Cola",
-              price: "150",
-              thumbnail: "empty"
-              },{
-              id:"4",
-              title: "Pepsi",
-              price: "170",
-              thumbnail: "empty"
-              },{
-              id:"5",
-              title: "Sprite",
-              price: "200",
-              thumbnail: "empty"
-             }
-            ]);    
-*/
+main()

96a64746ed5d83120b1baf43d1dea0627154d104
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-17 17:56:59 -0300

    deletebyID, updatebyID, save funcionando

--- backend/desafioHBS/src/Containers/ContainerMemory.js
+++ backend/desafioHBS/src/Containers/ContainerMemory.js
@@ -7,6 +7,10 @@ class ContainerMemory {
     return this.elements;
   }
 
+  getById(id) {
+    return this.elements.filter((element) => element.id === parseInt(id));
+  }
+
   save(element) {
     element.id = !this.elements.length
       ? 1
@@ -15,6 +19,33 @@ class ContainerMemory {
     this.elements.push(element);
 
     return element;
+  }
+
+  updateById(id, newData) {
+    const elementIndex = this.elements.findIndex(
+      (element) => element.id === parseInt(id)
+    );
+
+    if (elementIndex === -1) return { error: true };
+
+    this.elements[elementIndex] = {
+      ...this.elements[elementIndex],
+      ...newData,
+    };
+
+    /* for (const key in newData) {
+      if (this.elements[key]) {
+        this.elements[key] = newData[key];
+      }
+    } */
+
+    return this.elements[elementIndex];
+  }
+
+  deleteById(id) {
+    const products = this.elements.filter((element) => element.id !== parseInt(id))
+    this.elements = products
+    return { success: true };
   }
 }
 
--- backend/desafioHBS/src/routers/ProductRouter.js
+++ backend/desafioHBS/src/routers/ProductRouter.js
@@ -10,6 +10,20 @@ productRouter.get("/", (req, res) => {
   res.send({ success: true, data: products });
 });
 
+productRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const product = ProductMemory.getById(id);
+  if (!product) {
+    res.send({
+      success: false,
+      data: undefined,
+      message: "Element not found",
+    });
+  } else {
+    res.send({ success: true, data: product });
+  }
+});
+
 productRouter.post("/", (req, res) => {
   const { title, price, thumbnail } = req.body;
 
@@ -17,5 +31,21 @@ productRouter.post("/", (req, res) => {
 
   res.send({ success: true, data: { id: product.id } });
 });
+
+productRouter.put("/:id",(req,res)=>{
+    const { id } = req.params;
+    const { title, price, thumbnail } = req.body;
+
+    const updatedProduct = ProductMemory.updateById(id, { title, price, thumbnail });
+
+    res.send({ success: true, data: { updated: updatedProduct } });
+})
+
+productRouter.delete("/:id",(req,res)=>{
+    const { id } = req.params;
+
+    const products = ProductMemory.deleteById(id);
+    res.send({ success: true, data: products });
+})
 
 export { productRouter };

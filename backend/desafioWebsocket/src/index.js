1f1f522cfaf749c53a5dd5a134c3ff754dce8aa7
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-23 16:42:59 -0300

    socket instalado

--- backend/desafioWebsocket/src/index.js
+++ backend/desafioWebsocket/src/index.js
@@ -2,13 +2,19 @@ import express from "express";
 import { productRouter } from "./routers/ProductRouter.js";
 import { ViewsRouter } from "./routers/ViewsRouter.js";
 import handlebars from "express-handlebars";
-import {Server as HttpServer} from 'http';
-import {Server as IOServer} from 'socket.io';
+import { Server as HttpServer } from "http";
+import { Server as IOServer } from "socket.io";
 
 const PORT = 8080;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+const server = new HttpServer(app);
+
+const io = new IOServer(server);
+
+io.on('connection', (socket)=>{
+    console.log(`Se ha conectado un usuario: ${socket.id}`);
+})
 
 app.engine(
   "hbs",
@@ -16,16 +22,15 @@ app.engine(
     extname: ".hbs",
     defaultLayout: "main.hbs",
   })
-);
-
-app.set("view engine", "hbs");
-app.set("views", "./views");
-
+  );
+  
+  app.set("view engine", "hbs");
+  app.set("views", "./public/templates");
+  
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api/productos", productRouter);
 app.use("/", ViewsRouter);
 
-const server = app.listen(PORT, () =>
-  console.log(`Server running on port: ${PORT}`)
-);
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
--- backend/desafioWebsocket/src/routers/ViewsRouter.js
+++ backend/desafioWebsocket/src/routers/ViewsRouter.js
@@ -4,18 +4,14 @@ import { ProductApi } from "../Api/ProductApi.js";
 const ViewsRouter = Router();
 
 ViewsRouter.get("/", (req, res) => {
-  res.render("form-products");
+  const products = ProductApi.getAll();
+  res.render("form-table-products", { productos: products });
 });
 
 ViewsRouter.post("/productos", (req, res) => {
   const { title, price, thumbnail } = req.body;
   ProductApi.save({ title, price, thumbnail });
   res.redirect("/");
-});
-
-ViewsRouter.get("/productos", (req, res) => {
-  const products = ProductApi.getAll();
-  res.render("table-products", { productos: products });
 });
 
 export { ViewsRouter };

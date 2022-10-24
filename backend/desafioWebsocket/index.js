59ba1b0c55c58dd9ee3d233d8dbd6adb09112fdb
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-10-24 18:30:20 -0300

    desafio almost done

--- backend/desafioWebsocket/index.js
+++ backend/desafioWebsocket/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const { Server: HttpServer } = require('http');
-const { Server: SocketIOServer } = require('socket.io');
+const express = require("express");
+const { Server: HttpServer } = require("http");
+const { Server: SocketIOServer } = require("socket.io");
 
 const Product = require("./models/product/product.model");
+const Message = require("./models/message/message.model");
 
 const app = express();
 const httpServer = new HttpServer(app);
@@ -12,14 +13,42 @@ const PORT = 8080;
 
 app.use(express.static("./public"));
 
-httpServer.listen(PORT, ()=>console.log(`Servidor escuchando en puerto: ${PORT}`));
+httpServer.listen(PORT, () =>
+    console.log(`Servidor escuchando en puerto: ${PORT}`)
+);
 
-io.on('connection', socket => {
-    bringAllProducts(socket)
-})
+io.on("connection", (socket) => {
+    bringAllProducts(socket);
+    bringAllMessages(socket);
 
-const bringAllProducts = async(socket) => {
-    const allProduct = await Product.getAll();    
+    socket.on("new product", (newProduct) => {
+        saveProduct(newProduct);
+    });
+
+    socket.on("new message", (message)=>{
+        saveMessage(message);
+    })
+});
+
+const bringAllProducts = async (socket) => {
+    const allProduct = await Product.getAll();
     socket.emit("all products", allProduct);
-    return allProduct;
+};
+
+
+const saveProduct = async (newProduct) => {
+    await Product.save(newProduct);
+    const allProduct = await Product.getAll();
+    io.sockets.emit("all products", allProduct);
+};
+
+const saveMessage = async (newMessage) => {
+    await Message.save(newMessage);
+    const allMessage = await Message.getAll();
+    io.sockets.emit("all messages", allMessage);
+};
+
+const bringAllMessages = async (socket) => {
+    const allMessage = await Message.getAll();
+    socket.emit("all messages", allMessage);
 };

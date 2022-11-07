9f5f5a517d9d09601420f11ced50b6f805410a71
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-11-07 17:07:11 -0300

    config sqlite3 para messages db

--- backend/desafioDatabase/utils/createMessagesTable.js
+++ backend/desafioDatabase/utils/createMessagesTable.js
@@ -1,24 +1,23 @@
 const knexOptions = {
-  client: "mysql",
-  connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
-    database: "desafiodb",
-  },
+  client: "sqlite3",
+  connection: { filename: "./db/ecommerce.sqlite" },
+  useNullAsDefault: true,
 };
 const knex = require("knex")(knexOptions);
 
 const createMessagesTable = () => {
-  knex.schema.createTable('messages', (table)=>{
-    table.increments('id'),
-    table.string('message'),
-    table.string('createdAt')
-  }).then((result) => {
-    console.log('Table: "messages", created successfully.');
-  }).catch((err) => {
-    console.log(`ERROR: ${err.sqlMessage}.`);
-  });
-}
+  knex.schema
+    .createTable("messages", (table) => {
+      table.increments("id"),
+        table.string("message"),
+        table.string("createdAt");
+    })
+    .then((result) => {
+      console.log('Table: "messages", created successfully.');
+    })
+    .catch((err) => {
+      console.log(`ERROR: ${err.sqlMessage}.`);
+    });
+};
 
 module.exports = createMessagesTable;
